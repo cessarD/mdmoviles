34f607864f2b68b0b262dfb3c6138826e894b280
Mabel <00007415@uca.edu.sv>
2020-05-22 15:00:42 -0600

    first commit

---
+++ app/src/main/java/com/caceres06/modulosmoviles/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.caceres06.modulosmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
